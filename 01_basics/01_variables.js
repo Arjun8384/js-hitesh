0aa53de373c5e2621321897d77c3da66cd6f5404
Arjun8384 <146354638+Arjun8384@users.noreply.github.com>
2025-06-26 04:22:17 +0000

    font size changed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+//";" at the end of a line is not necessary in JS
+const accId = 134343    //accId is just a variable, just try to name it something readble
+let accMail = "arjun@gmail.com";
+var accPass = "12324"
+actCity = "Jaipur"
+let accState  // allowed to not filling any value in it, only declaration
+// accId = 2 It is not allowed to declare without giving any type let/var/const
